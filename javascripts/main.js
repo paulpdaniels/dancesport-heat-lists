0097b32c1c3c84cd9b160c6747ae0957419f313c
paulpdaniels <paulpdaniels@gmail.com>
2015-02-02 21:47:39 -0800

    * Moved pages into partials

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,11 +2,35 @@
  * Created by Paul on 1/31/2015.
  */
 
-;(function (root) {
+;
+(function (root) {
     'use strict';
-    var module = angular.module('dancesportHeatLists', ['ui.bootstrap']);
+    var module = angular.module('dancesportHeatLists', ['ui.bootstrap', 'ngRoute']);
 
-    module.controller('CompetitionController', function($scope, $http, $q){
+    module.config(function ($routeProvider) {
+        $routeProvider
+            .when('/about', {templateUrl: 'partials/about.html', controller: 'AboutController'})
+            .when('/search', {templateUrl: 'partials/search.html', controller: 'CompetitionController'})
+            .otherwise('/search');
+    });
+
+    module.controller('AboutController', function ($scope) {
+
+    });
+
+    module.controller('NavigationController', function($scope, $rootScope, $location) {
+
+        $scope.activeItem = '/search';
+
+
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $scope.activeItem = $location.url();
+        });
+
+    });
+
+
+    module.controller('CompetitionController', function ($scope, $http, $q) {
 
         $scope.competitions = [];
         $scope.selectedCompetition = null;
@@ -14,7 +38,7 @@
         $scope.selectedEvent = null;
 
         $http.get('competitions/manifest.json')
-            .success(function(data, status, headers, config){
+            .success(function (data, status, headers, config) {
                 if (status === 200) {
                     var manifest = angular.fromJson(data);
                     $scope.competitions = manifest.competitions;
@@ -23,7 +47,7 @@
 
                 if ($scope.selectedCompetition) {
                     $http.get('competitions/' + $scope.selectedCompetition.key + '.json')
-                        .success(function(data, status, headers, config){
+                        .success(function (data, status, headers, config) {
                             if (status === 200) {
                                 $scope.events = angular.fromJson(data).events;
                                 $scope.selectedEvent = $scope.events[0];
@@ -34,7 +58,7 @@
 
         $scope.lookup = "";
 
-        $scope.select = function(item, model, label) {
+        $scope.select = function (item, model, label) {
 
             var i = 0;
             for (var len = $scope.events.length; i < len; ++i) {
